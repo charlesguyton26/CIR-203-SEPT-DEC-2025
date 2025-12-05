a158590b18aefcb389327d1a137bf72b5b18b10f
charlesguyton26 <charlesguyton26@gmail.com>
2025-12-05 10:35:49 +0300

    Add files via upload

---
+++ linked.py
@@ -0,0 +1,10 @@
+LinkedDict = {
+    'Name': ['Stal', 'Bilo', 'Charles', 'Guyton', 'Sil', 'Tya'],
+    'AdmNo': ['CIS 214', 'CIS 252', 'CIT 107', 'CIT 756', 'CIS 925', 'CIS 394'],
+    'Grades': {
+        'CIR 203': [80, 90, 70, 89, 60, 70],
+        'CIR 205': [50, 60, 70, 80, 90, 56],
+        'CIT 211': [100, 90, 80, 70, 60, 50]
+    },
+    'Next': None
+}
